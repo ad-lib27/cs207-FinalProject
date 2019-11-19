0696e4ed96630de0991b080440fee5e7e01b2afb
Camille M. Bean <beancamille98@Camilles-MacBook-3.local>
2019-11-19 16:29:49 -0500

    interact.py interface and test

--- adlib/interact.py
+++ adlib/interact.py
@@ -1,30 +1,41 @@
-from elem_function import exp, sin, cos, tan
+from adlib.elem_function import exp, sin, cos, tan
 from adlib.autodiff import AutoDiffToy as ADT
 
-def main():
+def getfunc():
     #dummy x-value for checking function validity 
     x=0
-
-    #allowing repeated attempts for successful function entry
-    while True:
-        #collects function from user input, checks for validity
-        #does not check if function is composed of functions outside of milestone scope
-        func = input("Enter a Python-formatted function f(x) for evaluation:")
-        try: 
-            exec(func)
-        except:
-            print("This is not a valid function, please retry.")
-        else: break
+    #collects function from user input, checks for validity
+    #does not check if function is composed of functions outside of milestone scope
+    func = input("Enter a Python-formatted function f(x) for evaluation:")
+    try: 
+        exec(func)
+    except:
+        print("This is not a valid function, please retry.")
+        return False
+    else: 
+        return func
     #allowing repeated attempts for x-value entry
-    while True:
-        try: x = float(input("Enter an x-value to evaluate, where x is a real scalar:"))
-        except ValueError:
-            print("This is not a valid x-value, please retry.")
-        else: break
 
-    #prints value and derivative of result
-    x = ADT(x)
-    result = eval(func)
-    print (f"Function Value: {result.val} Function Derivative: {result.der}")
+def getxvalue():
+    try: x = float(input("Enter an x-value to evaluate, where x is a real scalar:"))
+    except ValueError:
+            print("This is not a valid x-value, please retry.")
+            return False
+    else:
+        return x
+
+def main():
+    gf = getfunc()
+    while gf == False:
+        gf = getfunc()
+    print("Successful function entry!")
+    gx = getxvalue()
+    while gx == False:
+        gx = getxvalue()
+    print("Successful x-value entry!")
+    x = ADT(gx)
+    result = eval(gf)
+    print(f"Function Value: {result.val} Function Derivative: {result.der}")      
+
 if __name__ == '__main__':
     main()
---
+++ tests/test_interact.py
@@ -0,0 +1,20 @@
+import unittest
+from adlib.interact import getfunc, getxvalue
+import mock
+import pytest
+
+def test_bad_function():
+    with mock.patch('builtins.input', return_value="3x"):
+        assert getfunc() == False
+    
+def test_good_function():
+    with mock.patch('builtins.input', return_value="3*x+5"):
+        assert getfunc() == "3*x+5"
+
+def test_bad_value():
+    with mock.patch('builtins.input', return_value="hello"):
+        assert getxvalue() == False
+
+def test_good_value():
+    with mock.patch('builtins.input', return_value="2"):
+        assert getxvalue() == 2
